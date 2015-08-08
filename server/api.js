5aa212da2208edca0002a4c4c31a467921d74042
Edwin Zhang <ehzhang@mit.edu>
2015-08-08 14:19:06 -0700

    add example api route

---
+++ server/api.js
@@ -0,0 +1,7 @@
+Router.route('/restful', {where: 'server'})
+  .get(function () {
+    this.response.end('get request\n');
+  })
+  .post(function () {
+    this.response.end('post request\n');
+  });
