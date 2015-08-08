7a64c0e85bce36d2d7dd2432b4d840573e03a7ab
Edwin Zhang <ehzhang@mit.edu>
2015-08-08 14:28:52 -0700

    api example

--- server/api.js
+++ server/api.js
@@ -1,7 +1,21 @@
-Router.route('/restful', {where: 'server'})
-  .get(function () {
+var base = '/api/';
+
+var api = [
+{
+  endpoint: 'restful',
+  get: function(){
     this.response.end('get request\n');
-  })
-  .post(function () {
+  },
+  post: function(){
     this.response.end('post request\n');
-  });
+  }
+}
+];
+
+// Build the API
+api.forEach(function(endpoint){
+  Router
+    .route(base + endpoint.endpoint, {where: 'server'})
+    .get(endpoint.get)
+    .post(endpoint.post);
+});
