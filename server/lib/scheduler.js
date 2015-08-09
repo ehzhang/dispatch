4f8a13fe29cf5b501358cde24926f79843a8c801
Anish Athalye <me@anishathalye.com>
2015-08-09 01:02:42 -0700

    Add reminder

--- server/lib/scheduler.js
+++ server/lib/scheduler.js
@@ -13,7 +13,7 @@ SyncedCron.add({
   name: 'Master scheduling algorithm',
   schedule: function(parser) {
     // parser is a later.parse object
-    return parser.text('every 5 seconds');
+    return parser.text('every 5 seconds'); // TODO increase this
   },
   job: function() {
     var tasks = Tasks.find({status: 'open'}).fetch();
